1541842b2d3332cd7c58421bc88d202904f33a0d
SalamBhai <saulasheriffdeen@gmail.com>
2022-11-27 23:29:08 +0100

    Changes for push

--- Application/Extensions/ServiceCollectionExtension.cs
+++ Application/Extensions/ServiceCollectionExtension.cs
@@ -20,8 +20,7 @@ public static class ServiceCollectionExtension
                    q.AddJobAndTrigger<SaveWeatherForecastInformationToDatabaseJob>();
                    q.AddJobAndTriggerForCronSchedule<SendMessageJob>();
                }
-             );
-        //      .AddQuartzHostedService(
-        //    q => q.WaitForJobsToComplete = true);
+             ).AddQuartzHostedService(
+          q => q.WaitForJobsToComplete = true);
     }
 }
